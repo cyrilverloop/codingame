be299f3a38567ce03e63a5b9f10a72a7e8a6af2f
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-02-09 11:35:09 +0100

    Indenting PSR-12.

--- src/Training/Medium/MayanCalculation/MayanCalculation.php
+++ src/Training/Medium/MayanCalculation/MayanCalculation.php
@@ -16,18 +16,15 @@ class MayanCalculation implements Puzzle
     public function execute($stdin): void
     {
         fscanf($stdin, "%d %d", $L, $H);
-        for ($i = 0; $i < $H; $i++)
-        {
+        for ($i = 0; $i < $H; $i++) {
             fscanf($stdin, "%s", $numeral);
         }
         fscanf($stdin, "%d", $S1);
-        for ($i = 0; $i < $S1; $i++)
-        {
+        for ($i = 0; $i < $S1; $i++) {
             fscanf($stdin, "%s", $num1Line);
         }
         fscanf($stdin, "%d", $S2);
-        for ($i = 0; $i < $S2; $i++)
-        {
+        for ($i = 0; $i < $S2; $i++) {
             fscanf($stdin, "%s", $num2Line);
         }
         fscanf($stdin, "%s", $operation);
