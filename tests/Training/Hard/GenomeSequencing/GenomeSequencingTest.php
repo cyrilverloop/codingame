42b62321349db1e64779a65dc0582f09a43a689b
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-02-12 10:13:02 +0100

    Adding tests for "Genome sequencing".

---
+++ tests/Training/Hard/GenomeSequencing/GenomeSequencingTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Training\Hard\GenomeSequencing;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Training\Hard\GenomeSequencing\GenomeSequencing;
+
+/**
+ * Tests for the "Genome sequencing" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Training\Hard\GenomeSequencing\GenomeSequencing
+ * @group genomeSequencing
+ */
+final class GenomeSequencingTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new GenomeSequencing();
+    }
+
+    /**
+     * Test that the code can be executed for "AACCTT".
+     *
+     * @group surface_AACCTT
+     */
+    public function testCanExecuteAACCTT(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - AACCTT.txt',
+            6 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "AGATTACAGA".
+     *
+     * @group surface_AGATTACAGA
+     */
+    public function testCanExecuteAGATTACAGA(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - AGATTACAGA.txt',
+            10 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "AACTT".
+     *
+     * @group surface_AACTT
+     */
+    public function testCanExecuteAACTT(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - AACTT.txt',
+            5 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "AGATTA".
+     *
+     * @group surface_AGATTA
+     */
+    public function testCanExecuteAGATTA(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - AGATTA.txt',
+            6 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Reversed AGATTA".
+     *
+     * @group surface_reversedAGATTA
+     */
+    public function testCanExecuteReversedAGATTA(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/05 - reversed AGATTA.txt',
+            6 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "ATCG".
+     *
+     * @group surface_ATCG
+     */
+    public function testCanExecuteReversedATCG(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/06 - ATCG.txt',
+            4 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "CCCTGACATGA".
+     *
+     * @group surface_CCCTGACATGA
+     */
+    public function testCanExecuteCCCTGACATGA(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/07 - CCCTGACATGA.txt',
+            11 . PHP_EOL
+        );
+    }
+}
