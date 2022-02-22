fc5a60969bff05cdb948f9d5941990c30f1bc21e
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-02-22 11:03:27 +0100

    Adding tests for "Rooks movements".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Added
 - Tests for "1000000000D world".
+- Tests for "Rooks movements".
 
 ## [3.4.0] - 2022-02-21
 ### Added
---
+++ tests/Community/Training/Easy/RooksMovements/CGTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Easy\RooksMovements;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Easy\RooksMovements\RooksMovements;
+
+/**
+ * Tests for the "Rooks movements" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Easy\RooksMovements\RooksMovements
+ * @group rooksMovements
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new RooksMovements();
+    }
+
+    /**
+     * Test that the code can be executed for "MOVING FREELY".
+     *
+     * @group rooksMovements_movingFreely
+     */
+    public function testCanExecuteMovingFreely(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - MOVING FREELY.txt',
+            file_get_contents(__DIR__ . '/output/01 - MOVING FREELY.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "CLOSE TO THE EDGE".
+     *
+     * @group rooksMovements_closeToTheEdge
+     */
+    public function testCanExecuteCloseToTheEdge(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - CLOSE TO THE EDGE.txt',
+            file_get_contents(__DIR__ . '/output/02 - CLOSE TO THE EDGE.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "ONLY ALLIES".
+     *
+     * @group rooksMovements_onlyAllies
+     */
+    public function testCanExecuteOnlyAllies(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - ONLY ALLIES.txt',
+            file_get_contents(__DIR__ . '/output/03 - ONLY ALLIES.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "FOR FRODOOO".
+     *
+     * @group rooksMovements_forFrodooo
+     */
+    public function testCanExecuteForFrodooo(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - FOR FRODOOO.txt',
+            file_get_contents(__DIR__ . '/output/04 - FOR FRODOOO.txt')
+        );
+    }
+}
