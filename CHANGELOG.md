1559bd3e504131fe92c61e0dd97f1661a5dae4fb
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-04-29 09:52:58 +0200

    Adding tests for "Feature extraction".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -34,6 +34,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Tests for "Is the king in check? (part 1)".
 - Tests for "A mountain of a mole hill".
 - Tests for "Where's Wally".
+- Tests for "Feature extraction".
 
 ## [3.7.0] - 2022-03-31
 ### Added
---
+++ tests/Community/Training/Easy/FeatureExtraction/CGTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Easy\FeatureExtraction;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Easy\FeatureExtraction\FeatureExtraction;
+
+/**
+ * Tests for the "Feature extraction" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Easy\FeatureExtraction\FeatureExtraction
+ * @group featureExtraction
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new FeatureExtraction();
+    }
+
+    /**
+     * Test that the code can be executed for "Test 1".
+     *
+     * @group featureExtraction_test1
+     */
+    public function testCanExecuteTest1(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - test 1.txt',
+            file_get_contents(__DIR__ . '/output/01 - test 1.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 2".
+     *
+     * @group featureExtraction_test2
+     */
+    public function testCanExecuteTest2(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - test 2.txt',
+            file_get_contents(__DIR__ . '/output/02 - test 2.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 3".
+     *
+     * @group featureExtraction_test3
+     */
+    public function testCanExecuteTest3(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - test 3.txt',
+            file_get_contents(__DIR__ . '/output/03 - test 3.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 4".
+     *
+     * @group featureExtraction_test4
+     */
+    public function testCanExecuteTest4(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - test 4.txt',
+            file_get_contents(__DIR__ . '/output/04 - test 4.txt')
+        );
+    }
+}
