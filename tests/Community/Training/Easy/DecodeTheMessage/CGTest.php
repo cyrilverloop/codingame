4bda009b1ea9780fdd2278b5b8e8bf7fda125f4c
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-06-25 10:34:58 +0200

    Adding tests for "Decode the message".

---
+++ tests/Community/Training/Easy/DecodeTheMessage/CGTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Easy\DecodeTheMessage;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Easy\DecodeTheMessage\DecodeTheMessage;
+
+/**
+ * Tests for the "Decode the message" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Easy\DecodeTheMessage\DecodeTheMessage
+ * @group decodeTheMessage
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new DecodeTheMessage();
+    }
+
+    /**
+     * Test that the code can be executed for "Test 1".
+     *
+     * @group decodeTheMessage_test1
+     */
+    public function testCanExecuteTest1(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - test 1.txt',
+            "ja" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 2".
+     *
+     * @group decodeTheMessage_test2
+     */
+    public function testCanExecuteTest2(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - test 2.txt',
+            "qxs" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 3".
+     *
+     * @group decodeTheMessage_test3
+     */
+    public function testCanExecuteTest3(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - test 3.txt',
+            "ADe" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 4".
+     *
+     * @group decodeTheMessage_test4
+     */
+    public function testCanExecuteTest4(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - test 4.txt',
+            "410" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 5".
+     *
+     * @group decodeTheMessage_test5
+     */
+    public function testCanExecuteTest5(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/05 - test 5.txt',
+            "565440" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 6".
+     *
+     * @group decodeTheMessage_test6
+     */
+    public function testCanExecuteTest6(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/06 - test 6.txt',
+            "Hello ! World" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 7".
+     *
+     * @group decodeTheMessage_test7
+     */
+    public function testCanExecuteTest7(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/07 - test 7.txt',
+            "Awesome !" . PHP_EOL
+        );
+    }
+}
