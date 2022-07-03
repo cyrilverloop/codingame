84970349c9ca6958eda286d04ded23344d190a40
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-07-03 12:30:48 +0200

    Adding tests for "Kiss the girls".

---
+++ tests/Community/Training/Easy/KissTheGirls/CGTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Easy\KissTheGirls;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Easy\KissTheGirls\KissTheGirls;
+
+/**
+ * Tests for the "Kiss the girls" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Easy\KissTheGirls\KissTheGirls
+ * @group kissTheGirls
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new KissTheGirls();
+    }
+
+    /**
+     * Test that the code can be executed for "Test 1".
+     *
+     * @group kissTheGirls_test1
+     */
+    public function testCanExecuteTest1(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - test 1.txt',
+            8 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 2".
+     *
+     * @group kissTheGirls_test2
+     */
+    public function testCanExecuteTest2(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - test 2.txt',
+            33 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 3".
+     *
+     * @group kissTheGirls_test3
+     */
+    public function testCanExecuteTest3(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - test 3.txt',
+            10 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 4".
+     *
+     * @group kissTheGirls_test4
+     */
+    public function testCanExecuteTest4(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - test 4.txt',
+            151 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 5".
+     *
+     * @group kissTheGirls_test5
+     */
+    public function testCanExecuteTest5(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/05 - test 5.txt',
+            150 . PHP_EOL
+        );
+    }
+}
