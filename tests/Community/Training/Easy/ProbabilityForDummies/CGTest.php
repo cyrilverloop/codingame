f0877e986476d64258fe48f2942815b5100c26b9
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-07-16 11:18:01 +0200

    Adding tests for "Probability for dummies".

---
+++ tests/Community/Training/Easy/ProbabilityForDummies/CGTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Easy\ProbabilityForDummies;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Easy\ProbabilityForDummies\ProbabilityForDummies;
+
+/**
+ * Tests for the "Probability for dummies" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Easy\ProbabilityForDummies\ProbabilityForDummies
+ * @group probabilityForDummies
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new ProbabilityForDummies();
+    }
+
+    /**
+     * Test that the code can be executed for "Test 1".
+     *
+     * @group probabilityForDummies_test1
+     */
+    public function testCanExecuteTest1(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - test 1.txt',
+            "92%" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 2".
+     *
+     * @group probabilityForDummies_test2
+     */
+    public function testCanExecuteTest2(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - test 2.txt',
+            "30%" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 3".
+     *
+     * @group probabilityForDummies_test3
+     */
+    public function testCanExecuteTest3(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - test 3.txt',
+            "11%" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 4".
+     *
+     * @group probabilityForDummies_test4
+     */
+    public function testCanExecuteTest4(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - test 4.txt',
+            "57%" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 5".
+     *
+     * @group probabilityForDummies_test5
+     */
+    public function testCanExecuteTest5(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/05 - test 5.txt',
+            "18%" . PHP_EOL
+        );
+    }
+}
