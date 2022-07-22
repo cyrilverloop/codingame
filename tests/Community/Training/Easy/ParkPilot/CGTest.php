a8d539147a425affd1fd9bff730c053e4be435e7
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-07-22 11:09:23 +0200

    Adding tests for "Park pilot".

---
+++ tests/Community/Training/Easy/ParkPilot/CGTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Easy\ParkPilot;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Easy\ParkPilot\ParkPilot;
+
+/**
+ * Tests for the "Park pilot" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Easy\ParkPilot\ParkPilot
+ * @group parkPilot
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new ParkPilot();
+    }
+
+    /**
+     * Test that the code can be executed for "Test 1".
+     *
+     * @group parkPilot_test1
+     */
+    public function testCanExecuteTest1(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - test 1.txt',
+            file_get_contents(__DIR__ . '/output/01 - test 1.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 2".
+     *
+     * @group parkPilot_test2
+     */
+    public function testCanExecuteTest2(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - test 2.txt',
+            file_get_contents(__DIR__ . '/output/02 - test 2.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 3".
+     *
+     * @group parkPilot_test3
+     */
+    public function testCanExecuteTest3(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - test 3.txt',
+            file_get_contents(__DIR__ . '/output/03 - test 3.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 4".
+     *
+     * @group parkPilot_test4
+     */
+    public function testCanExecuteTest4(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - test 4.txt',
+            file_get_contents(__DIR__ . '/output/04 - test 4.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 5".
+     *
+     * @group parkPilot_test5
+     */
+    public function testCanExecuteTest5(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/05 - test 5.txt',
+            file_get_contents(__DIR__ . '/output/05 - test 5.txt')
+        );
+    }
+}
