a1c7c1eb0af79553c51655dfa8493e879bf30d67
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-07-26 11:09:04 +0200

    Adding tests for "In stereo".

---
+++ tests/Community/Training/Easy/InStereo/CGTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Easy\InStereo;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Easy\InStereo\InStereo;
+
+/**
+ * Tests for the "In stereo" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Easy\InStereo\InStereo
+ * @group inStereo
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new InStereo();
+    }
+
+    /**
+     * Test that the code can be executed for "Example".
+     *
+     * @group inStereo_example
+     */
+    public function testCanExecuteExample(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - example.txt',
+            file_get_contents(__DIR__ . '/output/01 - example.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Lattice".
+     *
+     * @group inStereo_lattice
+     */
+    public function testCanExecuteLattice(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - lattice.txt',
+            file_get_contents(__DIR__ . '/output/02 - lattice.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Diamond".
+     *
+     * @group inStereo_diamond
+     */
+    public function testCanExecuteDiamond(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - diamond.txt',
+            file_get_contents(__DIR__ . '/output/03 - diamond.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "ViewFullscreen".
+     *
+     * @group inStereo_viewFullscreen
+     */
+    public function testCanExecuteViewFullscreen(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - viewFullscreen.txt',
+            file_get_contents(__DIR__ . '/output/04 - viewFullscreen.txt')
+        );
+    }
+}
