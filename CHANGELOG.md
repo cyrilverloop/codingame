7ddcccd7e94641acd96d549535e95fa7cfd5afb6
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-10-25 09:22:34 +0200

    Adding tests for "Game of life".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -29,6 +29,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Tests for "Byte pair encoding".
 - Tests for "Find the missing plus signs in addition".
 - Tests for "Magic stones".
+- Tests for "Game of life".
 
 ## [3.13.0] - 2022-09-30
 ### Added
---
+++ tests/Community/Training/Medium/GameOfLife/CGTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Medium\GameOfLife;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Medium\GameOfLife\GameOfLife;
+
+/**
+ * Tests for the "Game of life" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Medium\GameOfLife\GameOfLife
+ * @group gameOfLife
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new GameOfLife();
+    }
+
+    /**
+     * Test that the code can be executed for "3x3".
+     *
+     * @group gameOfLife_3x3
+     */
+    public function testCanExecute3x3(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - 3x3.txt',
+            file_get_contents(__DIR__ . '/output/01 - 3x3.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "3x3".
+     *
+     * @group gameOfLife_3x3_2
+     */
+    public function testCanExecute3x3_2(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - 3x3.txt',
+            file_get_contents(__DIR__ . '/output/02 - 3x3.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "26x5".
+     *
+     * @group gameOfLife_26x5
+     */
+    public function testCanExecute26x5(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - 26x5.txt',
+            file_get_contents(__DIR__ . '/output/03 - 26x5.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "1x100".
+     *
+     * @group gameOfLife_1x100
+     */
+    public function testCanExecute1x100(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - 1x100.txt',
+            file_get_contents(__DIR__ . '/output/04 - 1x100.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "100x1".
+     *
+     * @group gameOfLife_100x1
+     */
+    public function testCanExecute100x1(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/05 - 100x1.txt',
+            file_get_contents(__DIR__ . '/output/05 - 100x1.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "30x30".
+     *
+     * @group gameOfLife_30x30
+     */
+    public function testCanExecute30x30(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/06 - 30x30.txt',
+            file_get_contents(__DIR__ . '/output/06 - 30x30.txt')
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "1x1".
+     *
+     * @group gameOfLife_1x1
+     */
+    public function testCanExecute1x1(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/07 - 1x1.txt',
+            0 . PHP_EOL
+        );
+    }
+}
