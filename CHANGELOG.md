e548e6a675aca0d18e3df3f810b9e1dad8e1a39e
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-11-20 10:04:52 +0100

    Adding tests for "Anagrams".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -25,6 +25,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Tests for "Digit sum successor".
 - Tests for "Factorial vs exponential".
 - Tests for "Langton's ant".
+- Tests for "Anagrams".
 
 ### Fixed
 - Covers annotation for "The grand festival - II".
---
+++ tests/Community/Training/Medium/Anagrams/CGTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Medium\Anagrams;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Medium\Anagrams\Anagrams;
+
+/**
+ * Tests for the "Anagrams" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Medium\Anagrams\Anagrams
+ * @group anagrams
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new Anagrams();
+    }
+
+    /**
+     * Test that the code can be executed for "Test 1".
+     *
+     * @group anagrams_test1
+     */
+    public function testCanExecuteTest1(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - test 1.txt',
+            "END OF LINE" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 2".
+     *
+     * @group anagrams_test2
+     */
+    public function testCanExecuteTest2(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - test 2.txt',
+            "I KNOW KUNG FU" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 3".
+     *
+     * @group anagrams_test3
+     */
+    public function testCanExecuteTest3(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - test 3.txt',
+            "IM SORRY DAVE IM AFRAID I CANT DO THAT" . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 4".
+     *
+     * @group anagrams_test4
+     */
+    public function testCanExecuteTest4(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - test 4.txt',
+            file_get_contents(__DIR__ . '/output/04 - test 4.txt')
+        );
+    }
+}
