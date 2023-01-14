a286b5b81de3982799d966fec01949e3e7a73a74
Cyril VERLOOP <cyril.verloop@webqard.fr>
2023-01-14 10:57:12 +0100

    Adding tests for "Minimal number of swaps".

---
+++ tests/Community/Training/Medium/MinimalNumberOfSwaps/CGTest.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CyrilVerloop\Codingame\Tests\Community\Training\Medium\MinimalNumberOfSwaps;
+
+use CyrilVerloop\Codingame\Tests\PuzzleTest;
+use CyrilVerloop\Codingame\Community\Training\Medium\MinimalNumberOfSwaps\MinimalNumberOfSwaps;
+
+/**
+ * Tests for the "Minimal number of swaps" puzzle.
+ *
+ * @covers \CyrilVerloop\Codingame\Community\Training\Medium\MinimalNumberOfSwaps\MinimalNumberOfSwaps
+ * @group minimalNumberOfSwaps
+ * @medium
+ */
+final class CGTest extends PuzzleTest
+{
+    public function setUp(): void
+    {
+        $this->puzzle = new MinimalNumberOfSwaps();
+    }
+
+    /**
+     * Test that the code can be executed for "Example".
+     *
+     * @group minimalNumberOfSwaps_example
+     */
+    public function testCanExecuteExample(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/01 - example.txt',
+            1 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 2".
+     *
+     * @group minimalNumberOfSwaps_test2
+     */
+    public function testCanExecuteTest2(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/02 - test 2.txt',
+            2 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 3".
+     *
+     * @group minimalNumberOfSwaps_test3
+     */
+    public function testCanExecuteTest3(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/03 - test 3.txt',
+            0 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 4".
+     *
+     * @group minimalNumberOfSwaps_test4
+     */
+    public function testCanExecuteTest4(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/04 - test 4.txt',
+            5 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 5".
+     *
+     * @group minimalNumberOfSwaps_test5
+     */
+    public function testCanExecuteTest5(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/05 - test 5.txt',
+            24 . PHP_EOL
+        );
+    }
+
+    /**
+     * Test that the code can be executed for "Test 6".
+     *
+     * @group minimalNumberOfSwaps_test6
+     */
+    public function testCanExecuteTest6(): void
+    {
+        $this->expectExecuteOutputAnswer(
+            __DIR__ . '/input/06 - test 6.txt',
+            121 . PHP_EOL
+        );
+    }
+}
