e6f24a90e997dc07575a7496e1e630493f24aece
Cyril VERLOOP <cyril.verloop@webqard.fr>
2023-08-31 14:43:15 +0200

    Adding configuration and generated tests for "Defibrillators".

--- tests/Training/Easy/Defibrillators/CGTest.php
+++ tests/Training/Easy/Defibrillators/CGTest.php
@@ -6,14 +6,16 @@ namespace CyrilVerloop\Codingame\Tests\Training\Easy\Defibrillators;
 
 use CyrilVerloop\Codingame\Tests\PuzzleTestCase;
 use CyrilVerloop\Codingame\Training\Easy\Defibrillators\Defibrillators;
+use PHPUnit\Framework\Attributes as PA;
 
 /**
  * Tests for the "Defibrillators" puzzle.
- *
- * @covers \CyrilVerloop\Codingame\Training\Easy\Defibrillators\Defibrillators
- * @group defibrillators
- * @medium
  */
+#[
+    PA\CoversClass(Defibrillators::class),
+    PA\Group('defibrillators'),
+    PA\Medium
+]
 final class CGTest extends PuzzleTestCase
 {
     public function setUp(): void
@@ -23,53 +25,49 @@ final class CGTest extends PuzzleTestCase
 
     /**
      * Test that the code can be executed for "Example".
-     *
-     * @group defibrillators_example
      */
-    public function testCanExecuteExample(): void
+    #[PA\Group('defibrillators_example')]
+    public function testCanExecuteTestOnlyOneLetterE(): void
     {
         $this->expectExecuteOutputAnswer(
             __DIR__ . '/input/01 - example.txt',
-            "Maison de la Prevention Sante" . PHP_EOL
+            file_get_contents(__DIR__ . '/output/01 - example.txt')
         );
     }
 
     /**
      * Test that the code can be executed for "Exact position".
-     *
-     * @group defibrillators_exactPosition
      */
+    #[PA\Group('defibrillators_exactPosition')]
     public function testCanExecuteExactPosition(): void
     {
         $this->expectExecuteOutputAnswer(
             __DIR__ . '/input/02 - exact position.txt',
-            "Cimetiere Saint-Etienne" . PHP_EOL
+            file_get_contents(__DIR__ . '/output/02 - exact position.txt')
         );
     }
 
     /**
      * Test that the code can be executed for "Complete file".
-     *
-     * @group defibrillators_completeFile
      */
+    #[PA\Group('defibrillators_completeFile')]
     public function testCanExecuteCompleteFile(): void
     {
         $this->expectExecuteOutputAnswer(
             __DIR__ . '/input/03 - complete file.txt',
-            "Caisse Primaire d'Assurance Maladie" . PHP_EOL
+            file_get_contents(__DIR__ . '/output/03 - complete file.txt')
         );
     }
 
     /**
      * Test that the code can be executed for "Complete file 2".
-     *
-     * @group defibrillators_completeFile2
      */
+    #[PA\Group('defibrillators_completeFile2')]
     public function testCanExecuteCompleteFile2(): void
     {
         $this->expectExecuteOutputAnswer(
             __DIR__ . '/input/04 - complete file 2.txt',
-            "Amphitheatre d'O" . PHP_EOL
+            file_get_contents(__DIR__ . '/output/04 - complete file 2.txt')
         );
     }
 }
